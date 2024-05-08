11848ff4d09b9136eb8b28808385d8d10a0e4d13
Mark D. Butala <markbutala@intl.zju.edu.cn>
2024-05-08 18:39:07 +0800

    Cruft cleanup.

--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,6 @@
-// #include "estimation.hpp"
 #include "kalman_filter.hpp"
 
 int main(int argc, char** argv) {
-    // Eigen::Matrix2d A;
-    // A << 1, 2, 3, 4;
-
-    // Eigen::Map<Eigen::MatrixXd> A_mat(A.data(), A.rows(), A.cols());
-
-    // print_matrix(A_mat);
-
     estimation::KF kf(10);
 
     return 0;
